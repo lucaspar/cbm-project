de857536462ecf1a11d97f4ece7cab20cf15d403
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2021-03-28 14:33:15 -0400

    reading data and building the graph 	modified:   .gitignore 	new file:   driver.py 	new file:   src/data.py

---
+++ driver.py
@@ -0,0 +1,14 @@
+from src.data import load, build_graphs
+
+def main():
+    filename = './data/eucore.edges'
+    res = 5
+
+    data = load(filename)
+    graphs = build_graphs(data, resolution=res)
+
+    for idx in range(res):
+        print(f'{idx}\t{graphs[idx].order()}\t{graphs[idx].size()}')
+
+if __name__ == '__main__':
+    main()
