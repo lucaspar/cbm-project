1962900e2823fe187be4263b13036649735f3f08
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2021-03-28 15:55:44 -0400

    verbosity for the driver 	modified:   driver.py

--- driver.py
+++ driver.py
@@ -1,15 +1,16 @@
 from src.data import load, build_graphs
 
-def main():
+def main(verbose=False):
     filename = './data/eucore.edges'
     res = 5
 
     data = load(filename)
     graphs = build_graphs(data, resolution=res)
 
-    print('index\tnodes\tedges')
-    for idx in range(res):
-        print(f'{idx}\t{graphs[idx].order()}\t{graphs[idx].size()}')
+    if verbose:
+        print('index\tnodes\tedges')
+        for idx in range(res):
+            print(f'{idx}\t{graphs[idx].order()}\t{graphs[idx].size()}')
 
 if __name__ == '__main__':
-    main()
+    main(verbose=True)
