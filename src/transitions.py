38edc465fc8343cf483e12cd929589ed0b60cdc4
cedré <daniel.gonzalez.cedre@gmail.com>
2021-05-02 14:38:46 -0400

    parallel processing  Changes to be committed: 	modified:   src/transitions.py

--- src/transitions.py
+++ src/transitions.py
@@ -3,67 +3,87 @@ from networkx.algorithms import isomorphism as iso
 import graph_tool.all as gt
 from graph_tool.topology import isomorphism, subgraph_isomorphism
 
+from pqdm.processes import pqdm
+
+from multiprocessing import Pool, RLock
+
 from src.data import load_graph_pairs
 
-# bad function name -- should change
-#def load_graph_pairs():
-#    enums = {idx: load_enumerated_graphs(size=idx) for idx in range(2, 6)}
-#    transitions = [(g, h) for enum in enums.values() for g in enum for h in enum]
-#    return transitions
+def process_transitions(task):
+    ego, G, H, transitions = task
+
+    G_vp = G.new_vertex_property('bool')
+    H_vp = H.new_vertex_property('bool')
+
+    G_nbhd = {w for w in G.vertex(ego).out_neighbors()} | {ego}
+    H_nbhd = {w for w in H.vertex(ego).out_neighbors()} | {ego}
+
+    for v in G.vertices():
+        if v in G_nbhd:
+            G_vp[v] = True
+        else:
+            G_vp[v] = False
+    for v in H.vertices():
+        if v in H_nbhd:
+            H_vp[v] = True
+        else:
+            H_vp[v] = False
+
+    G_egonet = gt.GraphView(G, vfilt=G_vp, directed=False)
+    H_egonet = gt.GraphView(H, vfilt=H_vp, directed=False)
+
+    #for idx, (size, pregraph, postgraph) in tqdm(enumerate(transitions), desc='COMPUTING ISOMORPHISMS', total=len(transitions), colour='red', leave=False):
+    for idx, (size, pregraph, postgraph) in enumerate(transitions):
+        assert pregraph.num_vertices() == size and postgraph.num_vertices() == size
+
+        G_iso = subgraph_isomorphism(pregraph, G_egonet, induced=True)
+        H_iso = subgraph_isomorphism(postgraph, H_egonet, induced=True)
+        continue
+
+        for z in G_egonet.vertices():
+            print(z)
+        print(G_iso[0].get_array())
+        for z in G_iso[0]:
+            print(z in G_egonet.vertices())
+        #print(g_iso, h_iso)
+
+    return
+
+def process_ego(task):
+    ego, graphs, transitions, cpus = task
+
+    tasks = [(ego, graphs[t], graphs[t+1], transitions) for t in range(len(graphs) - 1)]
+    result = pqdm(tasks, process_transitions, n_jobs=cpus//2, desc='PROCESSING TRANSITIONS', colour='red', leave=False)
+    return
+
+    #for t in tqdm(range(len(graphs) - 1), desc='ITERATING OVER TIMES', total=len(graphs)-1, colour='yellow', leave=False):
+    #for t in range(len(graphs) - 1):
+    #return
 
 # TODO
 # n: the number of vertices in the shared vertex set
 #   graph_tool ensures that a graph with n vertices has vertex names {0, 1, ... n-1}
 # graphs: a temporal sequence of graphs on a shared vertex set
+# cpus: max number of CPU cores available for parallel processing
 # empty: decides whether the empty (i.e., no edges) subgraph is considered
-def egonet_transitions(n, graphs, empty=True):
+def egonet_transitions(n, graphs, cpus=8, empty=True):
     transitions = load_graph_pairs(sizes=[2, 3, 4, 5])
     counts = [0 for _ in transitions]
-    for ego in tqdm(range(n), desc='ITERATING OVER NODES', total=n):
-        for t in tqdm(range(len(graphs) - 1), desc='ITERATING OVER TIMES', total=len(graphs)-1, leave=False):
-            g = graphs[t]
-            h = graphs[t+1]
 
-            g_vp = g.new_vertex_property('bool')
-            h_vp = h.new_vertex_property('bool')
+    tasks = [(ego, graphs, transitions, cpus//2) for ego in range(n)]
 
-            g_nbhd = {w for w in g.vertex(ego).out_neighbors()} | {ego}
-            h_nbhd = {w for w in h.vertex(ego).out_neighbors()} | {ego}
+    result = pqdm(tasks, process_ego, n_jobs=cpus//2, desc='PROCESSING NODES', colour='green')
 
-            for v in g.vertices():
-                if v in g_nbhd:
-                    g_vp[v] = True
-                else:
-                    g_vp[v] = False
-            for v in h.vertices():
-                if v in h_nbhd:
-                    h_vp[v] = True
-                else:
-                    h_vp[v] = False
+    return
 
-            g_egonet = gt.GraphView(g, vfilt=g_vp, directed=False)
-            h_egonet = gt.GraphView(h, vfilt=h_vp, directed=False)
+    tqdm.set_lock(RLock())
 
-            for idx, (size, pregraph, postgraph) in tqdm(enumerate(transitions), desc='COMPUTING ISOMORPHISMS', total=len(transitions), leave=False):
-                assert pregraph.num_vertices() == size and postgraph.num_vertices() == size
+    pool = Pool(cpus//4, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),))
+    pool.map(process_ego, tasks)
+    return
 
-                g_iso = subgraph_isomorphism(pregraph, g_egonet, induced=True)
-                h_iso = subgraph_isomorphism(postgraph, g_egonet, induced=True)
-                continue
-
-                for z in g_egonet.vertices():
-                    print(z)
-                print(g_iso[0].get_array())
-                for z in g_iso[0]:
-                    print(z in g_egonet.vertices())
-                #print(g_iso, h_iso)
-                exit()
-
-            continue
-            exit()
-
-            #print(len(g_nbhd), g_egonet.num_vertices())
-            #print(len(h_nbhd), h_egonet.num_vertices())
+    #for ego in tqdm(range(n), desc='ITERATING OVER NODES', total=n, colour='green'):
+    for ego in range(n):
 
         pass
     return
