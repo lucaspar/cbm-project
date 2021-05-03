c9207609a6e69b7fecc1d3a68b7b361c91b65b54
cedré <daniel.gonzalez.cedre@gmail.com>
2021-05-03 08:58:30 -0400

    bug fix  Changes to be committed: 	modified:   driver.py 	modified:   src/transitions.py

--- driver.py
+++ driver.py
@@ -8,12 +8,16 @@ def main(verbose=False):
     n, data = load(dataset)
     graphs = build_graphs(n, data, resolution=resolution)
 
-    result = egonet_transitions(n, graphs)
-
     if verbose:
         print('index\tnodes\tedges')
         for idx, g in enumerate(graphs):
             print(idx, g)
 
+    embeddings = egonet_transitions(n, graphs, cpus=32)
+
+    with open('embeddings.txt', 'w') as outfile:
+        for (ego, embedding) in embeddings:
+            outfile.write(f'{ego}\t{embedding}\n')
+
 if __name__ == '__main__':
-    main(verbose=True)
+    main()
--- src/transitions.py
+++ src/transitions.py
@@ -1,11 +1,9 @@
-from tqdm import tqdm
-from networkx.algorithms import isomorphism as iso
+import numpy as np
 import graph_tool.all as gt
 from graph_tool.topology import isomorphism, subgraph_isomorphism
 
+from tqdm import tqdm
 from pqdm.processes import pqdm
-
-from multiprocessing import Pool, RLock
 
 from src.data import load_graph_pairs
 
@@ -32,29 +30,35 @@ def process_transitions(task):
     G_egonet = gt.GraphView(G, vfilt=G_vp, directed=False)
     H_egonet = gt.GraphView(H, vfilt=H_vp, directed=False)
 
-    #for idx, (size, pregraph, postgraph) in tqdm(enumerate(transitions), desc='COMPUTING ISOMORPHISMS', total=len(transitions), colour='red', leave=False):
-    for idx, (size, pregraph, postgraph) in enumerate(transitions):
+    #for idx, (size, pregraph, postgraph) in enumerate(transitions):
+    for idx, (size, pregraph, postgraph) in tqdm(enumerate(transitions), desc='ISOMORPHISMS', total=len(transitions), colour='red', leave=False):
         assert pregraph.num_vertices() == size and postgraph.num_vertices() == size
 
         G_iso = subgraph_isomorphism(pregraph, G_egonet, induced=True)
         H_iso = subgraph_isomorphism(postgraph, H_egonet, induced=True)
-        continue
 
-        for z in G_egonet.vertices():
-            print(z)
-        print(G_iso[0].get_array())
-        for z in G_iso[0]:
-            print(z in G_egonet.vertices())
+        if len(G_iso) > 0 and len(H_iso) > 0:
+            for phi in G_iso:
+                for psi in H_iso:
+                    if np.array_equal(phi.get_array(), psi.get_array()):
+                        counts[idx] += 1
+
+    print(sum(counts))
+    return counts
+
+        #for z in G_egonet.vertices():
+        #    print(z)
+        #print(G_iso[0].get_array())
+        #for z in G_iso[0]:
+        #    print(z in G_egonet.vertices())
         #print(g_iso, h_iso)
-
-    return
 
 def process_ego(task):
     ego, graphs, transitions, cpus = task
 
     tasks = [(ego, graphs[t], graphs[t+1], transitions) for t in range(len(graphs) - 1)]
-    result = pqdm(tasks, process_transitions, n_jobs=cpus//2, desc='PROCESSING TRANSITIONS', colour='red', leave=False)
-    return
+    counts = pqdm(tasks, process_transitions, n_jobs=cpus//2, desc='TRANSITIONS', colour='yellow', leave=False)
+    return (ego, counts)
 
     #for t in tqdm(range(len(graphs) - 1), desc='ITERATING OVER TIMES', total=len(graphs)-1, colour='yellow', leave=False):
     #for t in range(len(graphs) - 1):
@@ -67,23 +71,23 @@ def process_ego(task):
 # cpus: max number of CPU cores available for parallel processing
 # empty: decides whether the empty (i.e., no edges) subgraph is considered
 def egonet_transitions(n, graphs, cpus=8, empty=True):
-    transitions = load_graph_pairs(sizes=[2, 3, 4, 5])
+    transitions = load_graph_pairs(sizes=list(range(2, 5)))
     counts = [0 for _ in transitions]
 
     tasks = [(ego, graphs, transitions, cpus//2) for ego in range(n)]
 
-    result = pqdm(tasks, process_ego, n_jobs=cpus//2, desc='PROCESSING NODES', colour='green')
+    embeddings = pqdm(tasks, process_ego, n_jobs=cpus//2, desc='NODES', colour='green')
 
-    return
+    return embeddings
 
-    tqdm.set_lock(RLock())
+    #tqdm.set_lock(RLock())
 
-    pool = Pool(cpus//4, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),))
-    pool.map(process_ego, tasks)
-    return
+    #pool = Pool(cpus//4, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),))
+    #pool.map(process_ego, tasks)
+    #return
 
-    #for ego in tqdm(range(n), desc='ITERATING OVER NODES', total=n, colour='green'):
-    for ego in range(n):
+    ##for ego in tqdm(range(n), desc='ITERATING OVER NODES', total=n, colour='green'):
+    #for ego in range(n):
 
-        pass
-    return
+    #    pass
+    #return
